4ac2c33cd7ddb905f76261293713f90e7e26530f
choresh <raanan34@gmail.com>
2024-03-27 08:13:53 -0700

    Phase 4.

--- data_processor.js
+++ data_processor.js
@@ -1,8 +1,14 @@
 const pg = require("pg");
+const SCHEMA_NAME = "public";
+const TABLE_NANE = "users_revenue";
 
 async function update() {
     const client = new pg.Client({password: "postgres", user: "postgres"});
     await client.connect();
+
+    const res = await client.query(`CREATE TABLE IF NOT EXISTS ${SCHEMA_NAME}.${TABLE_NANE} (user_id varchar, revenue integer)`)
+    console.log("Table was created if not exists yet", { res });
+    await client.end()
 }
 
 console.info("Update will started");
