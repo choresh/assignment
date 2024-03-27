ac65c4fb7d67ee41e3d69ae3dcba363c58b8bdf1
choresh <raanan34@gmail.com>
2024-03-28 00:48:39 +0200

    Phase 10.

--- data_processor.js
+++ data_processor.js
@@ -2,109 +2,144 @@ const pg = require("pg");
 const { createReadStream } = require("node:fs");
 const { createInterface } = require("node:readline");
 
-const DATABASE_NAME = "public";
-const PASSWORD ="postgres";
-const USER = "postgres";
-const SCHEMA_NAME = "public";
-const TABLE_NANE = "users_revenue";
-const FILE_PATH = "events.txt";
+class FsFacade {
+    static FILE_PATH = "events.txt";
 
-
-function getEventOperator(event) {
-    switch (event.name) {
-        case "add_revenue":
-            return '+';
-        case "subtract_revenue":
-            return '-';
-        default:
-            console.error("Invalid line", { event });
-            throw new Error("Invalid line");
+    static createLinesReader() {
+        return createInterface({
+            input: createReadStream(FsFacade.FILE_PATH),
+            crlfDelay: Infinity,
+        });
     }
 }
 
-async function updateDbFromLine(line, client) {
+class DbFacade {
+    static DATABASE_NAME = "public";
+    static PASSWORD ="postgres";
+    static USER = "postgres";
+    static SCHEMA_NAME = "public";
+    static TABLE_NANE = "users_revenue";
+    
+    static async create() {
+        let instance = new DbFacade()
+        instance._client = new pg.Client({ database: DbFacade.DATABASE_NAME, password: DbFacade.PASSWORD, user: DbFacade.USER });
+        await instance._client.connect();
+        return instance;
+    }
 
-    // Convers string to JSON object.
-    const event = JSON.parse(line);
+    async createTableAndIndexIfNotExists() {
+        await this._client.query(`CREATE TABLE IF NOT EXISTS ${DbFacade.SCHEMA_NAME}.${DbFacade.TABLE_NANE} (user_id varchar, revenue integer)`);
+        await this._client.query(`CREATE UNIQUE INDEX IF NOT EXISTS user_id_unique ON ${DbFacade.SCHEMA_NAME}.${DbFacade.TABLE_NANE} (user_id)`); // Index on 'user_id' will improve our SELECT clauses, and also - 'ON CONFLICT (user_id)' within the 'upsert' query (see method ' updateDbFromLine()') cannot work without such a definition.
+    }
 
-    // Get operator (+/-) which relevant for value of current event.
-    const eventOperator = getEventOperator(event);
+    async updateDbRow(event, eventOperator) {
+    
+        // Lock the row for update, to prevent other transactions from modifying it.
+        await this._client.query(`
+            SELECT * FROM ${DbFacade.SCHEMA_NAME}.${DbFacade.TABLE_NANE}
+            WHERE user_id = '${event.userId}'
+            FOR UPDATE;
+        `);
+    
+        // Perform 'upsert' (i.e. update or insert) of the relevnt row.
+        await this._client.query(`
+            INSERT INTO ${DbFacade.SCHEMA_NAME}.${DbFacade.TABLE_NANE} (user_id, revenue)
+            VALUES ('${event.userId}', ${eventOperator} ${event.value})
+            ON CONFLICT (user_id)
+            DO UPDATE SET revenue = ${DbFacade.SCHEMA_NAME}.${DbFacade.TABLE_NANE}.revenue ${eventOperator} ${event.value};
+        `);
+    }
 
-    // Lock the row for update, to prevent other transactions from modifying it.
-    await client.query(`
-        SELECT * FROM ${SCHEMA_NAME}.${TABLE_NANE}
-        WHERE user_id = '${event.userId}'
-        FOR UPDATE;
-    `);
+    async beginTransaction() {
+        await this._client.query("BEGIN");
+    }
 
-    // Perform 'upsert' (i.e. update or insert) of the relevnt row.
-    await client.query(`
-        INSERT INTO ${SCHEMA_NAME}.${TABLE_NANE} (user_id, revenue)
-        VALUES ('${event.userId}', ${eventOperator} ${event.value})
-        ON CONFLICT (user_id)
-        DO UPDATE SET revenue = ${SCHEMA_NAME}.${TABLE_NANE}.revenue ${eventOperator} ${event.value};
-    `);
-}
+    async commitTransaction() {
+        await this._client.query("COMMIT");
+    }
 
-async function updateDbFromFile(client) {
+    async rollbackTransaction() {
+        await this._client.query("ROLLBACK");
+    }
 
-    // The 'linesReader' enable us to read the file content in 'line by line' manner.
-    const linesReader = createInterface({
-        input: createReadStream(FILE_PATH),
-        crlfDelay: Infinity,
-    });
-
-    // Deal with events of the lines reader.
-    return new Promise(async (resolve, reject) => {
-        linesReader.on("close", () => {
-            // * File's lines reading ended, resolve the promise.
-            // * The 'setTimeout()' is workaround, to deal with bizarre behavior of the lines reader ('close' event triggered before last 'line' event).
-            setTimeout(resolve, 1000);
-        });
-        linesReader.on("error", (err) => {
-            // File's lines reading failed, reject the promise.
-            reject(err);
-        });
-        linesReader.on("line", async (line) => { 
-            // File's lines reader notify about current fetched line, create/update relevant row in DB.
-            await updateDbFromLine(line, client);
-        });
-    });
-}
-
-async function connectToDb() {
-    const client = new pg.Client({ database: DATABASE_NAME, password: PASSWORD, user: USER });
-    await client.connect();
-    return client;
-}
-
-async function createTableAndIndexIfNotExists(client) {
-    await client.query(`CREATE TABLE IF NOT EXISTS ${SCHEMA_NAME}.${TABLE_NANE} (user_id varchar, revenue integer)`);
-    await client.query(`CREATE UNIQUE INDEX IF NOT EXISTS user_id_unique ON ${SCHEMA_NAME}.${TABLE_NANE} (user_id)`); // Index on 'user_id' will improve our SELECT clauses, and also - 'ON CONFLICT (user_id)' within the 'upsert' query (see method ' updateDbFromLine()') cannot work without such a definition.
-}
-
-async function update() {
-    let client;
-    try {
-        client = await connectToDb();
-        await client.query("BEGIN"); // Begin transaction.
-        await createTableAndIndexIfNotExists(client);   
-        await updateDbFromFile(client);
-        await client.query("COMMIT"); // Commit transaction.
-    } catch (err) {
-        if (client) {
-            await client.query("ROLLBACK"); // Rollback transaction.
+    async destroy() {
+        if (this._client) {
+            await this._client.end();
         }
-        throw err;
-    } finally {
-        if (client) {
-            await client.end();
+    }
+}
+
+class DataProcessor {
+
+    static async update() {
+        let dbFacade;
+        try {
+            dbFacade = await DbFacade.create();
+            await dbFacade.beginTransaction();
+            await dbFacade.createTableAndIndexIfNotExists();   
+            await DataProcessor._updateDbFromFile(dbFacade);
+            await dbFacade.commitTransaction();
+        } catch (err) {
+            if (dbFacade) {
+                await dbFacade.rollbackTransaction();
+            }
+            throw err;
+        } finally {
+            if (dbFacade) {
+                await dbFacade.destroy();
+            }
         }
+    }
+
+    static _getEventOperator(event) {
+        switch (event.name) {
+            case "add_revenue":
+                return '+';
+            case "subtract_revenue":
+                return '-';
+            default:
+                console.error("Invalid line", { event });
+                throw new Error("Invalid line");
+        }
+    }
+
+    static async _updateDbFromLine(line, dbFacade) {
+
+        // Convers string to JSON object.
+        const event = JSON.parse(line);
+
+        // Get operator (+/-) which relevant for value of current event.
+        const eventOperator = DataProcessor._getEventOperator(event);
+
+        await dbFacade.updateDbRow(event, eventOperator);
+    }
+
+    static async _updateDbFromFile(dbFacade) {
+
+        // The 'linesReader' enable us to read the file content in 'line by line' manner.
+        const linesReader = FsFacade.createLinesReader();
+
+        // Deal with events of the lines reader.
+        return new Promise(async (resolve, reject) => {
+            linesReader.on("close", () => {
+                // * File's lines reading ended, resolve the promise.
+                // * The 'setTimeout()' is workaround, to deal with bizarre behavior of the lines reader ('close' event triggered before last 'line' event).
+                setTimeout(resolve, 1000);
+            });
+            linesReader.on("error", (err) => {
+                // File's lines reading failed, reject the promise.
+                reject(err);
+            });
+            linesReader.on("line", async (line) => { 
+                // File's lines reader notify about current fetched line, create/update relevant row in DB.
+                await DataProcessor._updateDbFromLine(line, dbFacade);
+            });
+        });
     }
 }
 
 console.info("Update started");
-update()
+DataProcessor.update()
     .then(() => {
         console.info("Update ended");
     })
